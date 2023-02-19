4fcaece68dd6877238d359626297d2539d03d9c9
akashhok17 <124417096+akashhok17@users.noreply.github.com>
2023-02-19 18:49:17 +0530

    Update info.py

--- info.py
+++ info.py
@@ -75,17 +75,17 @@ URL_SHORTENR_WEBSITE = environ.get('URL_SHORTENR_WEBSITE', 'api.shareus.in/short
 URL_SHORTNER_WEBSITE_API = environ.get('URL_SHORTNER_WEBSITE_API', '')
 
      # Auto Delete For Group Message (Self Delete) #
-SELF_DELETE_SECONDS = int(environ.get('SELF_DELETE_SECONDS', 300))
+SELF_DELETE_SECONDS = int(environ.get('SELF_DELETE_SECONDS', 180))
 SELF_DELETE = environ.get('SELF_DELETE', True)
 if SELF_DELETE == "True":
     SELF_DELETE = True
 
     # Download Tutorial Button #
 DOWNLOAD_TEXT_NAME = "📥 HOW TO DOWNLOAD 📥"
-DOWNLOAD_TEXT_URL = "https://youtube.com/shorts/_jmx8nMQrZY"
+DOWNLOAD_TEXT_URL = "https://t.me/+dEJuaJy9a41lMmJl"
 
    # Custom Caption Under Button #
 CAPTION_BUTTON = "Subscribe"
-CAPTION_BUTTON_URL = "https://youtube.com/@GreyMattersYT"
+CAPTION_BUTTON_URL = "https://t.me/allmovies4uindia"
 
    # Auto Delete For Bot Sending Files #
